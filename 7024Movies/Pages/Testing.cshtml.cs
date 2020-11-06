77a440bba77109a1940279f5d8e24594f4cf9790
devinmarie <70454758+devinmarie@users.noreply.github.com>
2020-11-06 14:46:16 -0500

    Discover Thriller, MovieId, and Testing Pages and classes.

--- 7024Movies/Pages/Testing.cshtml.cs
+++ 7024Movies/Pages/Testing.cshtml.cs
@@ -1,9 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Serialization;
 
 namespace _7024Movies.Pages
 {
@@ -11,6 +16,13 @@ namespace _7024Movies.Pages
     {
         public void OnGet()
         {
+            using (var webClient = new WebClient())
+            {
+                string JsonString = webClient.DownloadString("https://api.themoviedb.org/3/search/movie?api_key=a852a3b3771672da86800503084b853b&language=en-US&query=starwars&page=1&include_adult=false");
+                var results = SearchResults.FromJson(JsonString);
+                var results2 = results.Results;
+                ViewData["MovieResults"] = results2;
+            }
         }
     }
 }
