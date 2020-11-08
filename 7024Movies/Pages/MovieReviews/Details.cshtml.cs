68db982e518feea8d1c22f2fec7e944ab9cfff4d
Erikamagz <70440066+Erikamagz@users.noreply.github.com>
2020-11-07 19:52:09 -0500

    Model Edits

---
+++ 7024Movies/Pages/MovieReviews/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using _7024Movies.Data;
+using _7024Movies.Models;
+
+namespace _7024Movies.Pages.MovieReviews
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly _7024Movies.Data._7024MoviesContext _context;
+
+        public DetailsModel(_7024Movies.Data._7024MoviesContext context)
+        {
+            _context = context;
+        }
+
+        public MovieReview MovieReview { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            MovieReview = await _context.MovieReview.FirstOrDefaultAsync(m => m.MovieReviewId == id);
+
+            if (MovieReview == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
