29df91b3cee9e3d831de915b36b52c743394e1f2
magow <magow@DESKTOP-BU2O05O>
2020-11-15 19:23:13 -0500

    My code review suggestions

---
+++ 7024Movies/Pages/DiscoverOther.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _7024Movies.Pages
+{
+    public class DiscoverOtherModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
